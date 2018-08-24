9f2791b0a9d74a67b2daa443d56a5f6c2989d514
Ahmed Hamdan <ahmedhamdan9898@gmail.com>
2018-08-24 22:53:36 +0300

    Initial commit

---
+++ javaJoker/src/main/java/com/udacity/ahmed/javajoker/Joker.java
@@ -0,0 +1,9 @@
+package com.udacity.ahmed.javajoker;
+
+public class Joker {
+
+    public static String tellJoke() {
+        return "Microsoft gives you Windows, Linux gives you a home!";
+    }
+
+}
